98d3be7515215a35458aca5f2472261b7d24dfd7
camargosjessica <camargosjessica@hotmail.com>
2021-02-19 20:21:18 -0300

    first commmit

---
+++ CursoAluraCSharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoAluraCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, mundo");
+
+            Console.WriteLine("A execução acabou. Tecle enter para sair");
+            Console.ReadLine();
+        }
+    }
+}
